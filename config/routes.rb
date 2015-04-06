c39e8505bbe9396009509fd57df6612fb8fe0725
Bhaskar <bhaskar.kotu@coupa.com>
2015-04-06 23:38:34 +0530

    Adding visitors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :visitors, only: [:index, :create, :destroy], defaults: { format: :json }
+  root 'visitors#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
